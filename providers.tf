316dc09e9a0aab8f40047f56e86259a5876d8742
Laszlo M. Szabo <szabo.lacus.2001@gmail.com>
2025-05-25 22:27:59 +0200

    feat: cleanup provider.tf
    remove unneded data from account

--- providers.tf
+++ providers.tf
@@ -11,11 +11,3 @@ provider "hcloud" {
   # Configuration options
   token = var.hcloud_token
 }
-
-data "hcloud_ssh_key" "fedora_laptop_ssh_key" {
-  id = "28667665"
-}
-
-data "hcloud_primary_ip" "floating_ip" {
-  name = "primary_ip-1"
-}

7c546f0d92dd0a24af5c9e6add5f56009d3574cf
Laszlo M. Szabo <szabo.lacus.2001@gmail.com>
2025-05-25 21:51:48 +0200

    feat: working deployment with example tf file
    Fixed server.tf and examples to use the ssh key correctly

--- server.tf
+++ server.tf
@@ -1,13 +1,17 @@
+resource "hcloud_ssh_key" "main" {
+  name       = "my-ssh-key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
 resource "hcloud_server" "test" {
   name        = var.server_name
   image       = var.os_type
   server_type = var.server_type
   #  location    = var.location
-  ssh_keys = [data.id_ed22519_pub]
+  ssh_keys = [hcloud_ssh_key.main.id]
   labels = {
     type = "calibre"
   }
-  firewall_ids = var.firewall
+  firewall_ids =  [hcloud_firewall.calibre_firewall.id]
   datacenter   = var.datacenter
   public_net {
     ipv4_enabled = true
